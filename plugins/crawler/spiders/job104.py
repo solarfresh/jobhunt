0cd056d8a5af782dcad665e8127a39b161ad6939
黃上瑜 <shangyuhuang@gmail.com>
2020-10-09 10:55:02 +0800

    Crawler: to support follow link when parsing keyword search

--- plugins/crawler/spiders/job104.py
+++ plugins/crawler/spiders/job104.py
@@ -2,16 +2,21 @@ from datetime import datetime
 from scrapy import (Request, Spider)
 from scrapy.crawler import CrawlerProcess
 from typing import List
-from urllib.parse import quote
+from urllib.parse import (parse_qsl, quote, urlencode, urlparse, urlunparse)
 
 
 class Job104KeywordSpider(Spider):
     name = "job104"
 
     def __init__(self, keywords: List[str], *args, **kwargs):
-        year = datetime.now().year - 2
+        # year = datetime.now().year - 2
         self.start_urls = [
-            'https://www.104.com.tw/jobs/search/?ro=0&keyword={0}&jobsource={1}indexpoc'.format(quote(kw), year)
+            'https://www.104.com.tw/jobs/search/?ro=0' \
+            '&keyword={0}' \
+            '&jobsource=2018indexpoc' \
+            '&order=11' \
+            '&asc=0' \
+            '&page=1'.format(quote(kw))
             for kw in keywords]
         super(Job104KeywordSpider).__init__(*args, **kwargs)
 
@@ -20,13 +25,21 @@ class Job104KeywordSpider(Spider):
             yield Request(url=url, callback=self.parse)
 
     def parse(self, response):
+        item_nb = 0
         for article in response.css('article'):
+            update_at = article.css('div h2 span::text').get()
+            if not update_at:
+                continue
+
+            item_nb += 1
+            update_at = update_at.strip()
             # {'class': 'b-block--top-bord job-list-item b-clearfix js-job-item ', 'data-job-no': '10598582',
             #  'data-job-name': 'AI演算法工程師', 'data-job-ro': '1', 'data-cust-no': '22480876000',
             #  'data-cust-name': '大立光電股份有限公司', 'data-indcat': '1001004002', 'data-indcat-desc': '光學器材製造業',
             #  'data-is-save': '0', 'data-is-apply': '0', 'data-jobsource': '2018indexpoc'}
             attrib = article.attrib
             # print(article.css('div.b-block__left'))
+            print(update_at)
             job_link = article.css('div h2 a').attrib['href'].replace('//', 'https://')
             # todo: to obtain job name with article.css('div h2 a').get()
             company_info = article.css('div ul li a').attrib
@@ -38,6 +51,14 @@ class Job104KeywordSpider(Spider):
             print([tag for tag in article.css('div div.job-list-tag span::text').getall()])
             print(article.css('div.b-pos-relative a::text').get())
             # todo: should prepare item objects
+
+        # if item_nb:
+        #     url_info = urlparse(response.url)
+        #     query_params = dict(parse_qsl(url_info.query))
+        #     query_params['page'] = str(int(query_params['page']) + 1)
+        #     url_info = url_info._replace(query=urlencode(query_params))
+        #     next_url = urlunparse(url_info)
+        #     yield Request(url=next_url, callback=self.parse)
 
 
 if __name__ == '__main__':
